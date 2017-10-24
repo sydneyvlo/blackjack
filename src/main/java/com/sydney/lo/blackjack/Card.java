158cf8a01a21ab41dc511c514c9c76b22fde01b4
Sydney Lo <sydneyvlo@gmail.com>
2017-10-23 21:51:27 -0700

    Implemented the constructor for the Card class.

--- src/main/java/com/sydney/lo/blackjack/Card.java
+++ src/main/java/com/sydney/lo/blackjack/Card.java
@@ -1,4 +1,13 @@
 package com.sydney.lo.blackjack;
 
+import java.util.HashMap;
+
 public class Card {
+    // Member Variable
+    private CardType card;
+    private int numOfCardTypes = 13;
+
+    public Card(int cardId) {
+        card = CardType.fromInteger(cardId);
+    }
 }
