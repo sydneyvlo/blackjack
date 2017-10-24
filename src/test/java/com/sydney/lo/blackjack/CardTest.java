65640bd36f5e2f665ed82e0bca46fde2aa352092
Sydney Lo <sydneyvlo@gmail.com>
2017-10-23 22:44:44 -0700

    Imported the correct classe for JUnit 4. Removed old code that was meant for JUnit 3.8. Wrote one test.

--- src/test/java/com/sydney/lo/blackjack/CardTest.java
+++ src/test/java/com/sydney/lo/blackjack/CardTest.java
@@ -1,7 +1,15 @@
 package com.sydney.lo.blackjack;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
-public class CardTest extends TestCase {
 
+public class CardTest {
+
+    @Test
+    public void createCardTest() {
+        // Create a 2 card.
+        Card card = new Card(0);
+        assertEquals(0, card.getCardId());
+    }
 }
