8be7347f9a293dcadf0f7ec02cc80378f2ad7ead
Sydney Lo <sydneyvlo@gmail.com>
2017-10-24 14:42:03 -0700

    Wrote a simple test that checks to see that the generateNewDeck method generates the correct number of cards.

--- src/test/java/com/sydney/lo/blackjack/DeckTest.java
+++ src/test/java/com/sydney/lo/blackjack/DeckTest.java
@@ -1,5 +1,23 @@
+package com.sydney.lo.blackjack;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+import java.util.ArrayList;
 
 public class DeckTest {
+    @Test
+    public void generateNewDeckTest() {
+        Deck d = new Deck();
+        assertEquals(52, d.cardsLeft());
+    }
+
+    @Test
+    public void shuffleTest() {
+    }
+
+    @Test
+    public void dealCardTest() {
+    }
 
 }
