840ab0476d5f6954340f620528bbdfbdef41e60e
Sydney Lo <sydneyvlo@gmail.com>
2017-10-24 19:47:00 -0700

    Wrote a test that checks to see if the Dealer keeps hitting until his hand value is greater than 16.

--- src/test/java/com/sydney/lo/blackjack/DealerTest.java
+++ src/test/java/com/sydney/lo/blackjack/DealerTest.java
@@ -1,5 +1,22 @@
+package com.sydney.lo.blackjack;
+
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class DealerTest {
 
+    @Test
+    /**
+     * Makes sure the dealer hits when it has a hand value of 16 or less.
+     */
+    public void takeTurnTest() {
+        Dealer dealer = new Dealer();
+        Deck deck = new Deck();
+        deck.shuffle(1000);
+
+        while (dealer.getHandValue() <= 16) {
+            assertEquals("hit", dealer.takeTurn());
+            dealer.hit(deck.getCard());
+        }
+    }
 }
