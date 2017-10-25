a678af3871124a41be24a3dcf7ca28a6eebeed56
Sydney Lo <sydneyvlo@gmail.com>
2017-10-24 19:50:58 -0700

    Renamed Main to Game and then back to Main. Removed JUnit 3 code from MainTest file.

--- src/main/java/com/sydney/lo/blackjack/Main.java
+++ src/main/java/com/sydney/lo/blackjack/Main.java
@@ -4,8 +4,7 @@ package com.sydney.lo.blackjack;
  * Hello world!
  *
  */
-public class Main
-{
+public class Main {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
