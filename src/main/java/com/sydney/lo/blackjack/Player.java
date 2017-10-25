1edaae98cbb2b73c2b0d1dccf43eb79c668cac0f
Sydney Lo <sydneyvlo@gmail.com>
2017-10-24 17:02:26 -0700

    Implemented the Player class.

--- src/main/java/com/sydney/lo/blackjack/Player.java
+++ src/main/java/com/sydney/lo/blackjack/Player.java
@@ -1,4 +1,49 @@
 package com.sydney.lo.blackjack;
 
+import java.util.ArrayList;
+
 public class Player {
+
+    // Member variables
+    private ArrayList<Card> hand;
+    private int handValue;
+
+    /**
+     * Constructor initializes the member variables.
+     */
+    public Player() {
+        hand = new ArrayList<Card>();
+        handValue = 0;
+    }
+
+    /**
+     * Adds the Card object passed from the parameter into the ArrayList hand.
+     * Additionally, updates the hand's value appropriately.
+     *
+     * @param c Card object to be added to the hand.
+     */
+    public void hit(Card c) {
+        hand.add(c);
+
+        // Update the value of the hand
+        handValue += c.getCardValue();
+    }
+
+    /**
+     * Returns the value of the hand.
+     *
+     * @return Returns as an int, the value of the hand.
+     */
+    public int getHandValue() {
+        return handValue;
+    }
+
+    /**
+     * Returns the hand member variable.
+     *
+     * @return ArrayList of Card objects.
+     */
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
 }
