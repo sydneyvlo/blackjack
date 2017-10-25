a96272a3d527fed71d8a215db4377b078298ab80
Sydney Lo <sydneyvlo@gmail.com>
2017-10-24 17:47:21 -0700

    Implemented test for the takeTurn method that covers all three case of user input.

--- src/test/java/com/sydney/lo/blackjack/PlayerTest.java
+++ src/test/java/com/sydney/lo/blackjack/PlayerTest.java
@@ -3,6 +3,7 @@ package com.sydney.lo.blackjack;
 import org.junit.Test;
 import java.util.ArrayList;
 import static org.junit.Assert.*;
+import java.io.ByteArrayInputStream;
 
 public class PlayerTest {
 
@@ -32,6 +33,62 @@ public class PlayerTest {
         p1.hit(c);
 
         assertEquals(handValue + c.getCardValue(), p1.getHandValue());
+    }
+
+    @Test
+    /**
+     * Test whether the takeTurn function properly prompts the user for input and returns
+     * the correct actions based on that input. This case is for hit.
+     */
+    public void takeTurnHitTest() {
+        Player p1 = new Player();
+        // Change the input stream to be this stream below to simulate user input from the console.
+        ByteArrayInputStream in = new ByteArrayInputStream("h".getBytes());
+        System.setIn(in);
+
+        String action = p1.takeTurn();
+        assertEquals("hit", action);
+
+        // Change it back to the standard input stream.
+        System.setIn(System.in);
+    }
+
+    @Test
+    /**
+     * Test whether the takeTurn function properly prompts the user for input and returns
+     * the correct actions based on that input. This case is for stay.
+     */
+    public void takeTurnStayTest() {
+        Player p1 = new Player();
+
+        // Change the input stream.
+        ByteArrayInputStream in = new ByteArrayInputStream("s".getBytes());
+        System.setIn(in);
+
+        String action = p1.takeTurn();
+        assertEquals("stay", action);
+
+        // Change it back to the standard input stream.
+        System.setIn(System.in);
+    }
+
+    @Test
+    /**
+     * Test whether the takeTurn function properly prompts the user for input and returns
+     * the correct actions based on that input. This case is for random input.
+     */
+    public void takeTurnInvalidInputTest() {
+        Player p1 = new Player();
+
+        // Change the input stream.
+        ByteArrayInputStream in = new ByteArrayInputStream("asdfasdfasdf".getBytes());
+        System.setIn(in);
+
+        String action = p1.takeTurn();
+        assertEquals("stay", action);
+
+        // Change it back to the standard input stream.
+        System.setIn(System.in);
     }
 
 }
