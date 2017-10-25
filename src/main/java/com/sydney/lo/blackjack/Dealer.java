59afa259419df780049966d37169831f137ff04e
Sydney Lo <sydneyvlo@gmail.com>
2017-10-24 19:46:33 -0700

    Implemented the Dealer class.

--- src/main/java/com/sydney/lo/blackjack/Dealer.java
+++ src/main/java/com/sydney/lo/blackjack/Dealer.java
@@ -1,4 +1,20 @@
 package com.sydney.lo.blackjack;
 
-public class Dealer {
+public class Dealer extends Player {
+
+    public Dealer() {
+        super();
+    }
+
+    @Override
+    public String takeTurn() {
+        String action = "hit";
+
+        if (this.getHandValue() > 16) {
+            action = "stay";
+        }
+
+        return action;
+    }
+
 }
