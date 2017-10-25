f8fdc5c5a35b04db9c561f492a671919f37ece76
Sydney Lo <sydneyvlo@gmail.com>
2017-10-24 20:50:31 -0700

    Wrote code to prompt the user for input on whether not he/she wants to play the game.

--- src/main/java/com/sydney/lo/blackjack/Main.java
+++ src/main/java/com/sydney/lo/blackjack/Main.java
@@ -1,11 +1,28 @@
 package com.sydney.lo.blackjack;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class Main {
+    static String prompt = "Welcome to Soft Serve 16 Blackjack! Would you like to start a game (y/n)?: ";
+
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.print(prompt);
+        Scanner s = new Scanner(System.in);
+        String response = s.next();
+        boolean isPlaying = response.toLowerCase().equals("y");
+        Game game = new Game();
+
+        while (isPlaying) {
+            game.round();
+            System.out.print("Would you like to play again (y/n)?: ");
+            response = s.next();
+            isPlaying = response.toLowerCase().equals("y");
+        }
+
+        System.out.println("Thank you for playing! See you next time.");
     }
 }
